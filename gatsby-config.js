589fadab031a296f09016ed523ce97d2133f58af
Francis Labi <francislabu@Franciss-MacBook-Pro.local>
2021-09-22 21:14:40 +0000

    added auth

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,26 @@ module.exports = {
         apiToken: "d42887fbe2c6422de37b951076ea22",
       },
     },
-    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {
+          "/*": [
+            "Basic-Auth: someuser:somepassword anotheruser:anotherpassword",
+          ],
+          "/my-page": [
+            // matching headers (by type) are replaced by Gatsby Cloud with more specific routes
+            "Basic-Auth: differentuser:differentpassword",
+          ],
+
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 };
